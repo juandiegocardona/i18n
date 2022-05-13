116b24470eea7da0c9e87c19097130f08b457818
juandiegocardona <jd.cardonad@uniandes.edu.co>
2022-05-13 13:49:45 -0500

    avanceClase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const getLocale = ()=>{
+    return navigator.language || navigator.userLanguage;
+}
+
+getLocale();
+
+ReactDOM.render(
+	<IntlProvider 
+    locale={getLocale()}
+    messages={getLocale()==="es"? localeEsMessages : localeEnMessages}>
+        <JobsList/>
+	</IntlProvider>,
+    document.getElementById("root")
+);
